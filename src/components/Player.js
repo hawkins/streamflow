e6cc61d00f68976ab6355a62ceaf1d79efc3af55
Josh Hawkins <jkhaccounts@gmail.com>
2017-04-14 22:31:22 -0500

    Fix Twitch player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,30 +1,16 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
-import TwitchVideoEmbed from './Twitch';
 
 @observer class Player extends Component {
-  constructor() {
-    super();
-    this.state = {
-      height: 0,
-      width: 0
-    };
-  }
-  componentDidMount() {
-    const height = document.getElementById('content').clientHeight;
-    const width = document.getElementById('content').clientWidth;
-    this.setState({
-      height,
-      width
-    });
-  }
   render() {
-    const { width, height } = this.state;
+    const { channel } = this.props.store;
     return (
-      <TwitchVideoEmbed
-        channel={this.props.store.channel}
-        height={height}
-        width={width}
+      <iframe
+        src={`http://player.twitch.tv/?channel=${channel}`}
+        width="100%"
+        height="100%"
+        allowFullScreen="true"
+        frameBorder="0"
       />
     );
   }
