544b741e22be66931996b68be07eb9cf11be9745
Josh Hawkins <jkhaccounts@gmail.com>
2017-06-18 21:18:58 -0500

    Improve startup, add alwaysOnTop, default config

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -5,32 +5,36 @@ const fs = require("fs");
 const os = require("os");
 
 const CONFIG_FILE_PATH = `${os.homedir()}/.streamflow.json`;
-let config = undefined;
+const DEFAULT_CONFIG = {
+  favorites: ["cohhcarnage", "loserfruit", "koalibears", "aimbotcalvin"],
+  alwaysOnTop: true
+};
+
+let config;
 
 function loadConfig() {
-  // Load in config or create it if it does not exist
-  fs.exists(CONFIG_FILE_PATH, exists => {
-    if (exists) {
-      // Load the file
-      fs.readFile(CONFIG_FILE_PATH, (err, data) => {
-        if (err) {
-          throw err;
-        } else {
-          config = JSON.parse(data);
+  return new Promise((resolve, reject) => {
+    // Load in config or create it if it does not exist
+    fs.exists(CONFIG_FILE_PATH, exists => {
+      if (exists) {
+        // Load the file
+        fs.readFile(CONFIG_FILE_PATH, (err, data) => {
+          if (err) return reject(err);
+
+          // Add any new keys that did not exist the last time they saved config
+          config = Object.assign({}, DEFAULT_CONFIG, JSON.parse(data));
+
           console.log("Loaded config", config);
-          mainWindow.webContents.send("config", config);
-        }
-      });
-    } else {
-      // Write the file
-      config = {
-        favorites: ["cohhcarnage", "loserfruit", "koalibears", "aimbotcalvin"]
-      };
-      saveConfig(config, data => {
-        console.log("Wrote config", data);
-        mainWindow.webContents.send("config", data);
-      });
-    }
+          resolve(config);
+        });
+      } else {
+        // Write the file
+        config = Object.assign({}, DEFAULT_CONFIG);
+
+        saveConfig(config);
+        resolve(config);
+      }
+    });
   });
 }
 
@@ -39,6 +43,7 @@ function saveConfig(data, callback) {
     if (err) {
       throw err;
     } else {
+      console.log("Wrote config", config);
       if (callback) callback(data);
     }
   });
@@ -51,8 +56,10 @@ let mainWindow;
 function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 1024,
+    height: 768,
+    autoHideMenuBar: true,
+    alwaysOnTop: config.alwaysOnTop
   });
 
   // and load the index.html of the app.
@@ -72,40 +79,44 @@ function createWindow() {
     // when you should delete the corresponding element.
     mainWindow = null;
   });
-
-  // Handle client requesting user config
-  ipcMain.on("config request", loadConfig);
-
-  // Handle client requesting to save config
-  ipcMain.on("config save", (e, data) => {
-    saveConfig(data, data => {
-      config = data;
-      console.log("Wrote config", config);
-    });
-  });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+// Handle client requesting user config
+ipcMain.on("config request", () =>
+  mainWindow.webContents.send("config", config)
+);
 
-// Quit when all windows are closed.
-app.on("window-all-closed", function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+// Handle client requesting to save config
+ipcMain.on("config save", (e, data) => {
+  // Update config and add any missing keys
+  config = Object.assign({}, DEFAULT_CONFIG, config, data);
+  saveConfig(config);
 });
 
-app.on("activate", function() {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
+loadConfig()
+  .then(() => {
+    if (!mainWindow) createWindow();
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+    // This method will be called when Electron has finished
+    // initialization and is ready to create browser windows.
+    // Some APIs can only be used after this event occurs.
+    app.on("ready", createWindow);
+
+    // Quit when all windows are closed.
+    app.on("window-all-closed", function() {
+      // On OS X it is common for applications and their menu bar
+      // to stay active until the user quits explicitly with Cmd + Q
+      if (process.platform !== "darwin") {
+        app.quit();
+      }
+    });
+
+    app.on("activate", function() {
+      // On OS X it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (mainWindow === null) {
+        createWindow();
+      }
+    });
+  })
+  .catch(console.error);
