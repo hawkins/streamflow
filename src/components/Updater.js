481f802bddaed13d029a60d6268aae2410f8f49c
Josh Hawkins <jkhaccounts@gmail.com>
2017-08-23 12:47:20 -0500

    Improve updater dialog

--- src/components/Updater.js
+++ src/components/Updater.js
@@ -5,12 +5,19 @@ import { observer } from "mobx-react";
 
 @observer class Updater extends React.Component {
   actions = [
-    { label: "Not now.", onClick: () => null },
+    { label: "Not now.", onClick: this.hidePopup.bind(this) },
     { label: "Let's go!", onClick: this.handleUpdate.bind(this) }
   ];
 
   handleUpdate() {
     this.context.store.initiateUpdate();
+
+    this.hidePopup();
+  }
+
+  hidePopup() {
+    // Hide the popup
+    this.context.store.updateDownloaded = false;
   }
 
   render() {
@@ -18,10 +25,17 @@ import { observer } from "mobx-react";
       <Dialog
         actions={this.actions}
         active={this.context.store.updateDownloaded}
-        onEscKeyDown={() => null}
-        onOverlayClick={() => null}
+        onEscKeyDown={this.hidePopup.bind(this)}
+        onOverlayClick={this.hidePopup.bind(this)}
         title="New Update Downloaded"
-      />
+      >
+        <p>
+          We've downloaded a new update of Streamflow for you!
+        </p>
+        <p>
+          You can choose to restart the app to install this now, or cancel to wait until next time.
+        </p>
+      </Dialog>
     );
   }
 }
